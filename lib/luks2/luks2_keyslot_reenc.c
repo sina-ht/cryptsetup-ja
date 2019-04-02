cd33a4918177880c6769d103740c221ae6f519bb
Milan Broz <gmazyland@gmail.com>
2019-04-02 12:06:23 +0200

    Use common copyright line format.

--- lib/luks2/luks2_keyslot_reenc.c
+++ lib/luks2/luks2_keyslot_reenc.c
@@ -2,7 +2,7 @@
  * LUKS - Linux Unified Key Setup v2, reencryption keyslot handler
  *
  * Copyright (C) 2016-2018, Red Hat, Inc. All rights reserved.
- * Copyright (C) 2016-2018, Ondrej Kozina. All rights reserved.
+ * Copyright (C) 2016-2018, Ondrej Kozina
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
